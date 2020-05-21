e2200831448e164ccf3e4ccf3649bc1292809d78
elonzh <i@elonzhou.com>
2020-05-21 23:51:58 +0800

    update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-basedir="./robomaster/$(adb shell getprop ro.build.product)/$(adb shell getprop ro.build.date.utc)"
-echo "basedir: " ${basedir}
-mkdir -p ${basedir}
+basedir="$(pwd)/robomaster/$(adb shell getprop ro.product.name | tr -d '\r')/$(adb shell getprop ro.build.date.utc | tr -d '\r')"
+echo "basedir: ${basedir}"
+mkdir -p "${basedir}"
 
 echo "get netstat"
 adb shell netstat -p  > "${basedir}/netstat.txt"
@@ -10,10 +10,20 @@ adb shell netstat -p  > "${basedir}/netstat.txt"
 echo "get processes"
 adb shell ps  > "${basedir}/ps.txt"
 
-dirs=("/system" "/init.environ.rc" "/init.lc1860.3connective.rc" "/init.rc" "/init.trace.rc" "/init.usb.rc" "/ueventd.rc" "/data")
+#dirs=("/system" "/init.environ.rc" "/init.lc1860.3connective.rc" "/init.rc" "/init.trace.rc" "/init.usb.rc" "/ueventd.rc" "/data")
+dirs=("/system/firm/dji_scratch" "/system/etc/dji.json" "/system/bin" "/system/build.prop" "/init.environ.rc" "/init.lc1860.3connective.rc" "/init.rc" "/init.trace.rc" "/init.usb.rc" "/ueventd.rc")
 for path in ${dirs[*]}
 do
   echo "start pulling" "${path}"
   adb pull "${path}" "${basedir}${path}"
 done
 
+link="$(pwd)/robomaster/current"
+rm -vf "${link}"
+ln -sf "$(realpath "${basedir}")" "${link}"
+
+echo "add these paths to your PYTHONPATH:"
+echo "$(pwd)/robomaster/current/system/firm/dji_scratch/lib"
+echo "$(pwd)/robomaster/current/system/firm/dji_scratch/src/robomaster"
+echo "$(pwd)/robomaster/current/system/firm/dji_scratch/src/robomaster/custom_ui"
+echo "$(pwd)/robomaster/current/system/firm/dji_scratch/src/robomaster/multi_comm"
